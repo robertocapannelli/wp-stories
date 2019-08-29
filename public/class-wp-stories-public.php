09809df58074efc272db928b36814a2aa6ee2b0c
Roberto Capannelli <roberto.capannelli@gmail.com>
2019-08-29 16:10:44 +0200

    Initial commit

---
+++ public/class-wp-stories-public.php
@@ -0,0 +1,111 @@
+<?php
+
+/**
+ * The public-facing functionality of the plugin.
+ *
+ * @link       http://example.com
+ * @since      1.0.0
+ *
+ * @package    Wp_Stories
+ * @subpackage Wp_Stories/public
+ */
+
+/**
+ * The public-facing functionality of the plugin.
+ *
+ * Defines the plugin name, version, and two examples hooks for how to
+ * enqueue the public-facing stylesheet and JavaScript.
+ *
+ * @package    Wp_Stories
+ * @subpackage Wp_Stories/public
+ * @author     Your Name <email@example.com>
+ */
+class Wp_Stories_Public {
+
+	/**
+	 * The ID of this plugin.
+	 *
+	 * @since    1.0.0
+	 * @access   private
+	 * @var      string    $plugin_name    The ID of this plugin.
+	 */
+	private $plugin_name;
+
+	/**
+	 * The version of this plugin.
+	 *
+	 * @since    1.0.0
+	 * @access   private
+	 * @var      string    $version    The current version of this plugin.
+	 */
+	private $version;
+
+	/**
+	 * Initialize the class and set its properties.
+	 *
+	 * @since    1.0.0
+	 * @param      string    $plugin_name       The name of the plugin.
+	 * @param      string    $version    The version of this plugin.
+	 */
+	public function __construct( $plugin_name, $version ) {
+
+		$this->plugin_name = $plugin_name;
+		$this->version = $version;
+
+		add_shortcode('stories', [$this, 'render']);
+
+	}
+
+	/**
+	 * Register the stylesheets for the public-facing side of the site.
+	 *
+	 * @since    1.0.0
+	 */
+	public function enqueue_styles() {
+
+		/**
+		 * This function is provided for demonstration purposes only.
+		 *
+		 * An instance of this class should be passed to the run() function
+		 * defined in Wp_Stories_Loader as all of the hooks are defined
+		 * in that particular class.
+		 *
+		 * The Wp_Stories_Loader will then create the relationship
+		 * between the defined hooks and the functions defined in this
+		 * class.
+		 */
+
+		wp_enqueue_style( $this->plugin_name, plugin_dir_url( __FILE__ ) . 'css/wp-stories-public.css', array(), $this->version, 'all' );
+		wp_enqueue_style( 'zuck-js-style', plugin_dir_url( __FILE__ ) . 'css/zuck.css', array(), $this->version, 'all' );
+
+	}
+
+	/**
+	 * Register the JavaScript for the public-facing side of the site.
+	 *
+	 * @since    1.0.0
+	 */
+	public function enqueue_scripts() {
+
+		/**
+		 * This function is provided for demonstration purposes only.
+		 *
+		 * An instance of this class should be passed to the run() function
+		 * defined in Wp_Stories_Loader as all of the hooks are defined
+		 * in that particular class.
+		 *
+		 * The Wp_Stories_Loader will then create the relationship
+		 * between the defined hooks and the functions defined in this
+		 * class.
+		 */
+
+		wp_enqueue_script( 'zuck-js', plugin_dir_url( __FILE__ ) . 'js/zuck.js', false, $this->version, false );
+		wp_enqueue_script( $this->plugin_name, plugin_dir_url( __FILE__ ) . 'js/wp-stories-public.js', array( 'jquery' ), $this->version, true );
+
+	}
+
+	public function render() {
+		include_once 'partials/wp-stories-public-display.php';
+	}
+
+}
---
+++ public/partials/wp-stories-public-display.php
@@ -0,0 +1,17 @@
+<?php
+
+/**
+ * Provide a public-facing view for the plugin
+ *
+ * This file is used to markup the public-facing aspects of the plugin.
+ *
+ * @link       http://example.com
+ * @since      1.0.0
+ *
+ * @package    Wp_Stories
+ * @subpackage Wp_Stories/public/partials
+ */
+?>
+<div id="stories"></div>
+
+<!-- This file should primarily consist of HTML with a little bit of PHP. -->
